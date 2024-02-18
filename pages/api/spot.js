5f34211cd1c59834105da868b84a5c2686672a2d
Laurence Liang <laurence.w.liang@gmail.com>
2024-02-18 01:13:23 -0500

    Add basic REST API

--- pages/api/spot.js
+++ pages/api/spot.js
@@ -1,7 +1,9 @@
+let state = 0;
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        res.status(200).json({ message: 'Sent message to Spot.' })
+        state += 1;
+        res.status(200).json({ message: state })
     } else {
-        res.status(200).json({ message: 'This is Spot.' })
+        res.status(200).json({ message: state })
     }
   }
