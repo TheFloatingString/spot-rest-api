dee848b16abff704088afe2c996c8f2ba900c4b4
Laurence Liang <laurence.w.liang@gmail.com>
2024-02-18 01:32:07 -0500

    Add connection

--- pages/api/spot.js
+++ pages/api/spot.js
@@ -1,8 +1,9 @@
-let state = 0;
+let state = "rest";
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        state += 1;
-        res.status(200).json({ message: state })
+        let x = req.body;
+        state = x.data
+        res.status(200).json({ message: x })
     } else {
         res.status(200).json({ message: state })
     }
